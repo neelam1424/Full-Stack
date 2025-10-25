33443e67c978faecc3082885b02be5010884af1d
neelam1424 <neelammore1404@gmail.com>
2025-10-24 22:00:47 -0400

    operations

---
+++ 3_JavaScript/part1/operations.js
@@ -0,0 +1,30 @@
+
+let score = 102
+let bonus = 25
+
+let totalScore = score + bonus
+
+let addition = 4 + 5
+let substract = 9 - 3
+let mult = 3 * 5
+let divi =8 / 2
+let remainder = 9 % 2
+
+let power = 2 ** 4
+
+let myscore = 110
+myscore++
+
+
+let credit = 56
+credit--
+
+
+//comparision operations
+
+let num1 = 3
+let num2 = 3
+let num3 = 6
+
+console.log(num1 == num2);
+console.log(num1 != num3);
